2a83b07be10401328d911babb9a91cfc4692f029
Sabbirfeni <contact.sabbirbhuiyan@gmail.com>
2022-04-16 15:39:33 +0600

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// ********************************
+// Sidebar js
+// ********************************
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
